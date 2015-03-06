a192d429b20f3eccad7e3eb3d0c9789ed8416b12
tarrasque2501 <nyther2501@gmail.com>
2015-03-05 22:36:00 -0600

    añade a unitOfWork: TipodePagoRepository. edita: gestorTipoDePago

--- Visual Studio Project/eGym/BLL/GestorTipoDePago.cs
+++ Visual Studio Project/eGym/BLL/GestorTipoDePago.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using DAL;
 using EL;
 
@@ -11,6 +12,11 @@ namespace BLL
 {
     public class GestorTipoPago
     {
+        private UnitOfWork UoW = new UnitOfWork();
+        public IEnumerable<TipoDePago> listarPagos()
+        {
+            return UoW.TipoDePagoRepository.GetAll();
+        }
 
        
     }
