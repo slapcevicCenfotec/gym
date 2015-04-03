234c6027910b6872f4073aa580f90a898b5cc5b5
mauriciofer <mauriciofer@gmail.com>
2015-04-03 17:42:08 -0600

    updated version

---
+++ Visual Studio Project/eGym/ServicioEnClases/Scripts/buscarMusculos.js
@@ -0,0 +1,113 @@
+﻿var selected = [];
+var table;
+
+function buscarMusculos() {
+    var service = new ServicioEnClases.ServicioProyecto();
+      service.obtenerMusculos(onSuccessDioses, null, null);
+}
+
+function load() {
+    //var el = document.getElementById("tblDioses");
+    //el.addEventListener("click", function () { modificarMusculo($(this).find("td").eq(2).html()) }, false);
+
+    table = $('#tblDioses').DataTable();
+
+    $('#tblDioses tbody').on('click', 'tr', function () {
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    });
+}
+
+
+
+function modificarMusculo() {
+    
+    var rows = $('tr.selected');
+    var table = $('#tblDioses').DataTable();
+
+    var rowData = table.rows(rows).data();
+  
+    var id = rowData[0][0];
+    var nombre = rowData[0][1];
+    var ubicacion = rowData[0][2];
+    var origen = rowData[0][3];
+    var inserccion = rowData[0][4];
+    var inervacion = rowData[0][5];
+    var irrigacion = rowData[0][6];
+ 
+    var service2 = new ServicioEnClases.ServicioProyecto();
+
+    var datos = JSON.stringify({ pid: id, ppnombre: "prueba 5" ,pubicacion:ubicacion,porigen:origen,pinserccion:inserccion,pinervacion:inervacion,pirrigacion:irrigacion});
+
+    service2.modificarMusculo(datos, onSuccesModificar, errorMessage, null);
+
+}
+
+function eliminarMusculo() {
+    var rows = $('tr.selected');
+    var table = $('#tblDioses').DataTable();
+
+    var rowData = table.rows(rows).data();
+
+    var id = rowData[0][0];
+    
+    var service3 = new ServicioEnClases.ServicioProyecto();
+
+    //var datos = JSON.stringify({ pid: id});
+
+    service3.eliminarMusculo(id, onSuccesEliminar, errorMessage, null);
+
+}
+function ingresarMusculo() {
+    var nombre = $('#txtnombreMusculo').val(),
+         ubicacion = $('#txtubicacionMusculo').val(),
+         origen = $('#txtorigenMusculo').val(),
+         inserccion = $('#txtinserccionMusculo').val(),
+         inervacion = $('#txtinervacionMusculo').val(),
+         irrigacion = $('#txtirrigacionMusculo').val(),
+        service4 = new ServicioEnClases.ServicioProyecto();
+
+    var datos = JSON.stringify({ pnombre: nombre, pubicacion: ubicacion, porigen: origen, pinserccion: inserccion, pinervacion: inervacion, pirrigacion: irrigacion });
+    service4.insertarMusculo(datos, onSuccesIngresar, errorMessage, null, null);
+
+}
+
+function onSuccesModificar(result) {
+    alert('Se modificó correctamente el músculo');
+}
+function errorMessage(resul) {
+    alert(resul.get_message());
+}
+
+function onSuccesIngresar(result) {
+    alert('Se registró correctamente el demonio');
+}
+function onSuccesEliminar(result) {
+    alert('Se eliminó correctamente el Musculo');
+}
+
+
+function onSuccessDioses(result) {
+    var objeto = $.parseJSON(result);
+    var tbody = "";
+    $.each(objeto, function (i, item) {
+        tbody += '<tr>';
+        tbody += '<td>' + objeto[i].Id + '</td>';
+        tbody += '<td>' + objeto[i].Nombre + '</td>';
+        tbody += '<td>' + objeto[i].Ubicacion + '</td>';
+        tbody += '<td>' + objeto[i].Origen + '</td>';
+        tbody += '<td>' + objeto[i].Inserccion + '</td>';
+        tbody += '<td>' + objeto[i].Inervacion + '</td>';
+        tbody += '<td>' + objeto[i].Irrigacion + '</td>';
+
+        tbody += '</tr>';
+    });
+    $('#tblDioses tbody').append(tbody);
+    load();
+    //$('#divMostrarHuesos').removeClass('ocultar');
+}
---
+++ Visual Studio Project/eGym/ServicioEnClases/WebForm1.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ServicioEnClases.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+  
+    <link href="Style/jquery.dataTables.css" rel="stylesheet" />
+
+
+     <script src="Scripts/jquery-1.9.1.min.js"></script>
+     <script src="Scripts/bootstrap.min.js"></script>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" type="text/css" /> 
+    <script type="text/javascript" src="Scripts/jquery-1.10.1.min.js"></script>
+    <script type="text/javascript" src="Scripts/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="Scripts/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js">
+    </script>
+    <script type="text/javascript" src="Scripts/buscarMusculos.js">
+    </script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager runat="server">
+            <Services>
+                <asp:ServiceReference Path="~/ServicioProyecto.svc" />
+            </Services>
+        </asp:ScriptManager>
+    <div>
+     <input type="button" id="btnBuscarMusculos" onclick="buscarMusculos()" value="Buscar Musculos" />
+
+    <input type="button" id="btnModificarMusculo" onclick="modificarMusculo()" value="Modificar" />
+
+    <input type="button" id="btnEliminarMusculo" onclick="eliminarMusculo()" value="Eliminar" />
+
+    <table id="tblDioses" class="display">
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Nombre</td>
+                <td>Ubicacion</td>
+                <td>Origen</td>
+                <td>Inserccion</td>
+                <td>Inervacion</td>
+                <td>Irrigacion</td>
+            </tr>
+        </thead>
+        <tbody></tbody>   
+       
+    </table>
+    
+    <br />
+   
+  <div class="form-group">
+    <label for="">Nombre</label>
+    <input type="text" class="form-control" id="txtnombreMusculo" placeholder="Enter nombre">
+  </div>
+  <div class="form-group">
+    <label for="">Ubicacion</label>
+    <input type="text" class="form-control" id="txtubicacionMusculo" placeholder="Ubicacion">
+  </div>
+  <div class="form-group">
+     <label for="">Origen</label>
+    <input type="text" class="form-control" id="txtorigenMusculo" placeholder="Origen">
+  </div>
+     <div class="form-group">
+     <label for="">Insercccion</label>
+    <input type="text" class="form-control" id="txtinserccionMusculo" placeholder="Inserccion">
+  </div>
+  <div class="form-group">
+     <label for="">Inervacion</label>
+    <input type="text" class="form-control" id="txtinervacionMusculo" placeholder="Inervacion">
+  </div>
+   <div class="form-group">
+     <label for="">Irrigacion</label>
+    <input type="text" class="form-control" id="txtirrigacionMusculo" placeholder="Irrigacion">
+  </div>
+  <button type="button" class="btn btn-default" onclick="ingresarMusculo()">Submit</button>
+    </div>
+    </form>
+</body>
+</html>
