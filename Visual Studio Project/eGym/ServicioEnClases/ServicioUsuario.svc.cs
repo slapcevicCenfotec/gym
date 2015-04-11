976690e348dfb7e1c398b4e982fc29e7292916bb
Melvin Salas <melvinsalasrivera@gmail.com>
2015-04-11 15:04:46 -0600

    Se agrego el proyecto de UI-WEB

---
+++ Visual Studio Project/eGym/ServicioEnClases/ServicioUsuario.svc.cs
@@ -0,0 +1,29 @@
+﻿using BLL;
+using EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace ServicioEnClases
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ServicioUsuario
+    {
+        //Gestor gestor = new Gestor();
+        //[WebGet()]
+        //[OperationContract]
+        //public string obtenerUsuarios()
+        //{
+        //    //List<Usuario> usuarios = new List<Usuario>();
+        //    //usuarios = gestor.ListarUsuarios();
+        //    //return new JavaScriptSerializer()
+        //}
+    }
+}
