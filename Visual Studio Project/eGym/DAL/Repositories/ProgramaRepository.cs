7aa213640e3d2e919cb8a3a4e2e3f17085181cd5
slapcevicarkk <slapcevicm@ucenfotec.ac.cr>
2015-04-26 13:55:47 -0600

    cambios repositorio de programas

--- Visual Studio Project/eGym/DAL/Repositories/ProgramaRepository.cs
+++ Visual Studio Project/eGym/DAL/Repositories/ProgramaRepository.cs
@@ -70,7 +70,11 @@ namespace DAL.Repositories
                     {
                         Id = Convert.ToInt32(dr["ID"]),
                         IdUsuario = Convert.ToInt32(dr["USUARIO"]),
-                        //TipoAcondicionamiento = dr["TIPO_ACONDICIONAMIENTO    "],
+                        TipoAcondicionamiento = new TipoAcondicionamiento
+                        {
+                            Id = Convert.ToInt32(dr["ID_TIPO_ACONDICIONAMIENTO"]),
+                            Nombre = Convert.ToString(dr["NOMBRE_TIPO_ACONDICIONAMIENTO"])
+                        },
                         Estado = Convert.ToInt32(dr["ESTADO"]),
                     });
                 }
@@ -100,7 +104,10 @@ namespace DAL.Repositories
                         {
                             Id = Convert.ToInt32(dr["ID"]),
                             IdUsuario = Convert.ToInt32(dr["USUARIO"]),
-                            //TipoAcondicionamiento = dr["TIPO_ACONDICIONAMIENTO    "],
+                            TipoAcondicionamiento = new TipoAcondicionamiento{
+                                Id = Convert.ToInt32(dr["ID_TIPO_ACONDICIONAMIENTO"]),
+                                Nombre = Convert.ToString(dr["NOMBRE_TIPO_ACONDICIONAMIENTO"])
+                            },
                             Estado = Convert.ToInt32(dr["ESTADO"]),
                         };
                     }
@@ -172,6 +179,44 @@ namespace DAL.Repositories
             _insertItems.Clear();
             _deleteItems.Clear();
             _updateItems.Clear();
+        }
+
+        public Programa GetLast()
+        {
+
+            Programa programa = null;
+
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                DataSet ds = DBAccess.ExecuteSPWithDS(ref cmd, "SP_ObtenerUltimoPrograma");
+
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        programa = new Programa
+                        {
+                            Id = Convert.ToInt32(dr["ID"]),
+                            IdUsuario = Convert.ToInt32(dr["USUARIO"]),
+                            TipoAcondicionamiento = new TipoAcondicionamiento
+                            {
+                                Id = Convert.ToInt32(dr["ID_TIPO_ACONDICIONAMIENTO"]),
+                                Nombre = Convert.ToString(dr["NOMBRE_TIPO_ACONDICIONAMIENTO"])
+                            },
+                            Estado = Convert.ToInt32(dr["ESTADO"]),
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return programa;
         }
 
         private void InsertPrograma(Programa objPrograma)
