78532119e22a2da704e6aa065239d92db7e16eb3
unknown <sotoa6@CRHELAP054.global.baxter.com>
2015-04-27 22:53:36 -0600

    Agregando permisos de paginas

--- Visual Studio Project/eGym/UI-WEB/Roles/Modificar.aspx.vb
+++ Visual Studio Project/eGym/UI-WEB/Roles/Modificar.aspx.vb
@@ -2,7 +2,21 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        If Session("_USERPERMISSION") Is Nothing Then
+            Response.Redirect("http://localhost/egym/iniciarsesion.aspx")
+        End If
+        Dim permissions = TryCast(Session("_USERPERMISSION"), List(Of String))
+        Dim valido = False
+        For Each permission As String In permissions
+            If permission = "7" Then
+                valido = True
+            End If
+        Next
+        If Session("_USERID") Is Nothing Or Session("_USERID") = String.Empty Then
+            Response.Redirect("http://localhost/egym/iniciarsesion.aspx")
+        ElseIf valido = False Then
+            Response.Redirect("http://localhost/egym/iniciarsesion.aspx")
+        End If
     End Sub
 
 End Class
